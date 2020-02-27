0bcf1dfebabbf84c0b728b53808f85a82ad975d9
lvyou <376607092@qq.com>
2020-02-27 23:35:16 +0800

    搜索store

--- src/api/request.js
+++ src/api/request.js
@@ -1,34 +1,48 @@
 import { axiosInstance } from "./config";
 
 export const getBannerRequest = () => {
-  return axiosInstance.get ('/banner');
-}
+  return axiosInstance.get("/banner");
+};
 
 export const getRecommendListRequest = () => {
-  return axiosInstance.get ('/personalized');
-}
+  return axiosInstance.get("/personalized");
+};
 
 // console.log(axiosInstance,getBannerRequest,33333);
-export const getHotSingerListRequest = (count) => {
+export const getHotSingerListRequest = count => {
   return axiosInstance.get(`/top/artists?offset=${count}`);
-}
+};
 
-export const getSingerListRequest= (category, alpha, count) => {
-  return axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
-}
+export const getSingerListRequest = (category, alpha, count) => {
+  return axiosInstance.get(
+    `/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`
+  );
+};
 
-export const getRankListRequest=()=>{
+export const getRankListRequest = () => {
   return axiosInstance.get(`/toplist/detail`);
-}
+};
 
 export const getAlbumDetailRequest = id => {
   return axiosInstance.get(`/playlist/detail?id=${id}`);
 };
 
-export const getSingerInfoRequest=id=>{
+export const getSingerInfoRequest = id => {
   return axiosInstance.get(`/artists?id=${id}`);
-}
+};
 
-export const getLyricRequest=id=>{
+export const getLyricRequest = id => {
   return axiosInstance.get(`/lyric?id=${id}`);
-}
+};
+
+export const getHotKeyWordsRequest = () => {
+  return axiosInstance.get(`/search/hot`);
+};
+
+export const getSuggestListRequest = query => {
+  return axiosInstance.get(`/search/suggest?keywords=${query}`);
+};
+
+export const getResultSongsListRequest = query => {
+  return axiosInstance.get(`/search?keywords=${query}`);
+};
