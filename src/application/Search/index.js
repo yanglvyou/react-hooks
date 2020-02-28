4d84dc5deb4d076a9bd61cfb01102fca337a30e2
lvyou <376607092@qq.com>
2020-02-28 23:28:20 +0800

    搜索进行中

--- src/application/Search/index.js
+++ src/application/Search/index.js
@@ -1,28 +1,71 @@
-import React, { useState, useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { connect } from "react-redux";
 import { CSSTransition } from "react-transition-group";
-import SearchBox from './../../baseUI/search-box/index';
+import {
+  getHotKeyWords,
+  changeEnterLoading,
+  getSuggestList
+} from "./store/actionCreators";
+import SearchBox from "./../../baseUI/search-box/index";
 import { Container } from "./style";
+import Scroll from '../../baseUI/scroll';
 
 function Search(props) {
-   const [query,setQuery]=useState('');
+  const {
+    hotList,
+    enterLoading,
+    suggestList: immutableSuggestList,
+    songsCount,
+    songsList: immutableSongsList
+  } = props;
+
+  const suggestList = immutableSuggestList.toJS();
+  const songsList = immutableSongsList.toJS();
+
+  const {
+    getHotKeyWordsDispatch,
+    changeEnterLoadingDispatch,
+    getSuggestListDispatch,
+    getSongDetailDispatch
+  } = props;
+
+  const [query, setQuery] = useState("");
   //控制动画
   const [show, setShow] = useState(false);
 
   //由于是传递给子组件的，尽量用callback包裹，以使得在依赖未发生改变，始终给子组件相同的引用；
-  const searchBack=useCallback(()=>{
-    setShow(false)
-  },[]);
-
+  const searchBack = useCallback(() => {
+    setShow(false);
+  }, []);
 
   useEffect(() => {
     setShow(true);
   }, []);
 
-  const handleQuery=(q)=>{
-      console.log('q: ', q);
-      setQuery(q);
-  }
+  const handleQuery = q => {
+    console.log("q: ", q);
+    setQuery(q);
+  };
 
+  //当搜索框为空时
+  const renderHotKey = () => {
+    let list = hotList ? hotList.toJS() : [];
+    return (
+      <ul>
+        {list.map(item => {
+          return (
+            <li
+              className="item"
+              key={item.first}
+              onClick={() => setQuery(item.first)}
+            >
+              <span>{item.first}</span>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
 
   return (
     <CSSTransition
@@ -34,10 +77,41 @@ function Search(props) {
       onExited={() => props.history.goBack()}
     >
       <Container>
-        <SearchBox back={searchBack} newQuery={query} handleQuery={handleQuery}></SearchBox>
+        <SearchBox
+          back={searchBack}
+          newQuery={query}
+          handleQuery={handleQuery}
+        ></SearchBox>
       </Container>
     </CSSTransition>
   );
 }
 
-export default Search;
+//映射Redux全聚德state到组件的props上
+const mapStateToProps = state => ({
+  hotList: state.getIn(["search", "hotList"]),
+  enterLoading: state.getIn(["search", "enterLoading"]),
+  suggestList: state.getIn(["search", "suggestList"]),
+  songsCount: state.getIn(["player", "playList"]).size,
+  songsList: state.getIn(["search", "songsList"])
+});
+
+//映射dispatch到props 上
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getHotKeyWordsDispatch() {
+      dispatch(getHotKeyWords());
+    },
+    changeEnterLoadingDispatch(data) {
+      dispatch(changeEnterLoading(data));
+    },
+    getSuggestListDispatch(data) {
+      dispatch(getSuggestList(data));
+    }
+  };
+};
+
+//将UI组件包裹成容器组件
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Search));
